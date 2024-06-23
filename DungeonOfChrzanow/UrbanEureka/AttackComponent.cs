19d2e36f8c8ed66d496862a34289a57188342865
Moro347 <148942085+Moro347@users.noreply.github.com>
2024-06-23 20:18:20 +0200

    Update AttackComponent.cs

--- DungeonOfChrzanow/UrbanEureka/AttackComponent.cs
+++ DungeonOfChrzanow/UrbanEureka/AttackComponent.cs
@@ -11,16 +11,25 @@ class AttackComponent
 
     public bool IsTargetInRange(Point targetPosition)
     {
-        int distanceX = Math.Abs(positionComponent.Position.X -  targetPosition.X);
-        int distanceY = Math.Abs(positionComponent.Position.Y -  targetPosition.Y);
-        
+        int distanceX = Math.Abs(positionComponent.Position.X - targetPosition.X);
+        int distanceY = Math.Abs(positionComponent.Position.Y - targetPosition.Y);
 
-       
+
         return (distanceX <= range && distanceY == 0) || (distanceX == 0 && distanceY <= range);
     }
 
-    public void Attack(HealthComponent targetHealthComponent)
+    public void Attack(HealthComponent targetHealthComponent, GameObject npc)
     {
         targetHealthComponent.TakeDamage(strength);
+
+
+        if (targetHealthComponent.CurrentHealth <= 0)
+        {
+            npc.SetActive(false);
+        }
     }
 }
+
+
+
+
