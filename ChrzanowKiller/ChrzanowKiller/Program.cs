bdf85c70e46b14016ef56362c79465f5714e8db4
Milosz Mirek <148942065+SamochodNaSlonecznik@users.noreply.github.com>
2024-06-25 01:42:20 +0200

    UPLOADUJE TO CHYBA JUZ 10 RAZ
    Oczywiście zacznę jakie są problem:
    
        Jeden, ale nie musi to być problem bo jak będziemy za dużymi jełopami to zrobić lepiej to i tak mamy podstawę która "dostatecznie" działa
    
    
    
    Ale co działa?
    
        Da się chodzić po mapie
        Przez ściany się nie da przechodzić
        Fajnie bo @ się zmienia jak przechodzi na konkretny kolor celltype i wygląda jak by było oświetlenie
        Da się grać i się nie crashuje
    
    
    
    Dobra ale teraz jakie są problemy?
    
        Da się chodzić ale jest z tym związany inny problem, mapa jest trochę wymagająca i aktualny system chodzenia powoduje że mapa cała musi się regenerować od nowa za każdym razem gdy się ruszymy. I to skutkuje dość powolny poruszaniem. Naprawić to ejst pewnie prosty sposób a to jest zrobić to co na zajęciach czyli zamieniać ostatni celltype na jakim byliśmy na taki jak był przed wejściem na niego. Jednak nie byłem w stanie tego zrobić podczas robienia ruchu, więc jak się uda to ogarnąć to świetnie ale na razie mamy taki system chodzenia co działa.
    
    
    
    Co jest do zrobienia?
    
        NPC byłby fajny, ale nie wiem co z nim zrobić żeby za trudny nie był
        System Zbierania Kluczy i Monet
        System że po zebraniu Klucza możemy przejść przez czerwone drzwi
        I żeby system wysyłał wiadomość że "podniosłeś klucz/monete!" czy coś

---
+++ ChrzanowKiller/ChrzanowKiller/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Drawing;
+
+class Program
+{
+    static void Main()
+    {
+        Map map = new Map();
+        Point initialPosition = new Point(80, 16);
+        CellType[] walkableCellTypes = new CellType[] {
+            CellType.Floor,
+            CellType.Grass,
+            CellType.Diamond,
+            CellType.Door,
+            CellType.Shadow
+        };
+
+        Movement movement = new Movement(map, initialPosition, walkableCellTypes);
+        map.Display(movement.GetPlayerPosition());
+
+        while (true)
+        {
+            var key = Console.ReadKey(true).Key;
+            movement.MovePlayer(key);
+        }
+    }
+}
